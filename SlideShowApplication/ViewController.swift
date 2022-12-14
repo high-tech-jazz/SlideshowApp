19af41fe20796d28f090944f9ce704c51a702898
Takashi Ito <takashi_ito@stlining.com>
2022-12-14 15:23:02 +0900

    Initial Commit

---
+++ SlideShowApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideShowApplication
+//
+//  Created by 伊藤敬 on 2022/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
